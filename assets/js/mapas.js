acbd7a2239d71826bab727029510a4b3a4e8aa54
NardaC <nardac6@gmail.com>
2020-03-04 18:56:18 -0500

    init

---
+++ assets/js/mapas.js
@@ -0,0 +1,101 @@
+const contenedorMapaMiraflores = document.getElementById("contenedor-mapa-miraflores");
+const contenedorMapaIsidro = document.getElementById("contenedor-mapa-isidro");
+const contenedorMapaBarranco = document.getElementById("contenedor-mapa-barranco");
+const contenedorMapaSurco = document.getElementById("contenedor-mapa-surco");
+const mapa1 = document.getElementById("mapa1");
+const mapa2 = document.getElementById("mapa2");
+const mapa3 = document.getElementById("mapa3");
+const mapa4 = document.getElementById("mapa4");
+const mapa5 = document.getElementById("mapa5");
+const mapa6 = document.getElementById("mapa6");
+const mapa7 = document.getElementById("mapa7");
+const mapa8 = document.getElementById("mapa8");
+const mapa9 = document.getElementById("mapa9");
+const mapa10 = document.getElementById("mapa10");
+const mapa11 = document.getElementById("mapa11");
+const mapa12 = document.getElementById("mapa12");
+const mapa13 = document.getElementById("mapa13");
+const mapa14 = document.getElementById("mapa14");
+const mapa15 = document.getElementById("mapa15");
+const mapa16 = document.getElementById("mapa16");
+const mapa17 = document.getElementById("mapa17");
+ 
+
+//mostrar mapa miraflores
+mapa1.addEventListener("click",()=>{    
+    contenedorMapaMiraflores.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.7514753295504!2d-77.0321803496519!3d-12.129149446698628!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c81e36ee5f69%3A0xa6f25ab445179c2f!2sIbis%20Larco%20Miraflores!5e0!3m2!1ses!2spe!4v1583356060904!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+mapa2.addEventListener("click",()=>{
+    contenedorMapaMiraflores.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.9591597206977!2d-77.04932254965212!3d-12.114946946428889!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c830d1d1905f%3A0x1fe911dab7ff6374!2sPizza%20Rock!5e0!3m2!1ses!2spe!4v1583356135117!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+mapa3.addEventListener("click",()=>{
+    contenedorMapaMiraflores.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.039307575415!2d-77.05152304965222!3d-12.109461646324819!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c83617e32723%3A0xaaed87415758db17!2sJuicy%20Lucy%20-%20La%20Mar!5e0!3m2!1ses!2spe!4v1583355974252!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+mapa4.addEventListener("click",()=>{
+    contenedorMapaMiraflores.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.808754494358!2d-77.02561414965196!3d-12.12523404662424!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c9031079d23b%3A0xe3d668e292801b72!2sJuicy%20Lucy%20Leuro!5e0!3m2!1ses!2spe!4v1583356235937!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+mapa5.addEventListener("click",()=>{
+    contenedorMapaMiraflores.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.7423845337435!2d-77.0265888496518!3d-12.129770746710427!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c81d7c6a0971%3A0xa49cd932c4f6c926!2sJuicy%20Lucy%20-%20Balboa!5e0!3m2!1ses!2spe!4v1583357893772!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+mapa6.addEventListener("click",()=>{
+    contenedorMapaMiraflores.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.8687146843677!2d-77.03521404965197!3d-12.121134046546338!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c81876de5cad%3A0x7ecbddf25b4af7a7!2sJam%20Box%20Bar%20Cafe!5e0!3m2!1ses!2spe!4v1583357765757!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+mapa7.addEventListener("click",()=>{
+    contenedorMapaMiraflores.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.8259812006318!2d-77.029896949652!3d-12.124056246601882!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c81958b7d7df%3A0x45248ec993093c21!2sSelina%20Miraflores%20Lima!5e0!3m2!1ses!2spe!4v1583357227078!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+mapa8.addEventListener("click",()=>{
+    contenedorMapaMiraflores.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.843465104451!2d-77.01367804965192!3d-12.122860746579178!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8074683773f%3A0x238f3eed8efe0c14!2sSpizza!5e0!3m2!1ses!2spe!4v1583357331324!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+mapa9.addEventListener("click",()=>{
+    contenedorMapaMiraflores.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.837611001061!2d-77.03858734965199!3d-12.12326104658679!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c823b362ee57%3A0x58b7e9940d920c60!2sPitahaya%20Bar!5e0!3m2!1ses!2spe!4v1583357371319!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+
+mapa10.addEventListener("click",()=>{
+    contenedorMapaMiraflores.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.8023741395195!2d-77.0321140496519!3d-12.125670246632524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c81edce97ce5%3A0x12ec7f5e421e4984!2sStreet%20Burger!5e0!3m2!1ses!2spe!4v1583356837377!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+//mostrar mapa san isidro
+
+mapa11.addEventListener("click",()=>{
+    contenedorMapaIsidro.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.0759228439993!2d-77.04022104951937!3d-12.106954891385177!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c840b81a59d3%3A0x83eb3e92da13e84f!2sBon%20Beef%20Burger%20-%20Pardo%20y%20Aliaga!5e0!3m2!1ses!2spe!4v1583361430537!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+mapa12.addEventListener("click",()=>{
+    contenedorMapaIsidro.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.0759228439993!2d-77.04022104951937!3d-12.106954891385177!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c840b81a59d3%3A0x83eb3e92da13e84f!2sBon%20Beef%20Burger%20-%20Pardo%20y%20Aliaga!5e0!3m2!1ses!2spe!4v1583361430537!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+//mostrar mapa barranco
+
+mapa13.addEventListener("click",()=>{
+    contenedorMapaBarranco.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.4571732467293!2d-77.02489334951896!3d-12.149247291356875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105b7ed82ec6b0f%3A0xb560404c906413fe!2sSantos!5e0!3m2!1ses!2spe!4v1583361892988!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+mapa14.addEventListener("click",()=>{
+    contenedorMapaBarranco.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.452109733459!2d-77.02559344951898!3d-12.149592791356708!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105b7eda0fc30d9%3A0x9d6e05c1d1c2d0f6!2sLa%20Posada%20Del%20Mirador!5e0!3m2!1ses!2spe!4v1583361978351!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+mapa15.addEventListener("click",()=>{
+    contenedorMapaBarranco.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.571756828914!2d-77.02461464951905!3d-12.141426291362151!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105b7eff94f50b5%3A0x4c8207ce430e6a12!2sLa%20Cuadra%20de%20Salvador!5e0!3m2!1ses!2spe!4v1583362031349!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+//mostrar mapa barranco
+
+mapa16.addEventListener("click",()=>{
+    contenedorMapaSurco.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.393659531221!2d-76.97737994951959!3d-12.085180491399708!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c7ad784a96bd%3A0x8e0b3487046fc6f3!2sJuicy%20Lucy%20Jockey!5e0!3m2!1ses!2spe!4v1583362447251!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+mapa17.addEventListener("click",()=>{
+    contenedorMapaSurco.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.424744217262!2d-76.97090014951962!3d-12.083048191401115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c7ab38e317eb%3A0x84f82cc5c8668f9a!2sBon%20Beef%20Burger!5e0!3m2!1ses!2spe!4v1583362811439!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+
+
