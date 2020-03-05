50ca4330f75399eae25cc8b9657730755d9ddb52
NardaC <nardac6@gmail.com>
2020-03-04 20:39:35 -0500

    cambios

--- assets/js/mapas.js
+++ assets/js/mapas.js
@@ -2,6 +2,11 @@ const contenedorMapaMiraflores = document.getElementById("contenedor-mapa-mirafl
 const contenedorMapaIsidro = document.getElementById("contenedor-mapa-isidro");
 const contenedorMapaBarranco = document.getElementById("contenedor-mapa-barranco");
 const contenedorMapaSurco = document.getElementById("contenedor-mapa-surco");
+const contenedorMapaBorja = document.getElementById("contenedor-mapa-borja");
+const contenedorMapaSurquillo = document.getElementById("contenedor-mapa-surquillo");
+const contenedorMapaVictoria = document.getElementById("contenedor-mapa-victoria");
+const contenedorMapacercado = document.getElementById("contenedor-mapa-cercado");
+
 const mapa1 = document.getElementById("mapa1");
 const mapa2 = document.getElementById("mapa2");
 const mapa3 = document.getElementById("mapa3");
@@ -19,6 +24,10 @@ const mapa14 = document.getElementById("mapa14");
 const mapa15 = document.getElementById("mapa15");
 const mapa16 = document.getElementById("mapa16");
 const mapa17 = document.getElementById("mapa17");
+const mapa18 = document.getElementById("mapa18");
+const mapa19 = document.getElementById("mapa19");
+const mapa20 = document.getElementById("mapa20");
+const mapa21 = document.getElementById("mapa21");
  
 
 //mostrar mapa miraflores
@@ -83,19 +92,42 @@ mapa14.addEventListener("click",()=>{
     contenedorMapaBarranco.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.452109733459!2d-77.02559344951898!3d-12.149592791356708!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105b7eda0fc30d9%3A0x9d6e05c1d1c2d0f6!2sLa%20Posada%20Del%20Mirador!5e0!3m2!1ses!2spe!4v1583361978351!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
 })
 
-mapa15.addEventListener("click",()=>{
-    contenedorMapaBarranco.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.571756828914!2d-77.02461464951905!3d-12.141426291362151!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105b7eff94f50b5%3A0x4c8207ce430e6a12!2sLa%20Cuadra%20de%20Salvador!5e0!3m2!1ses!2spe!4v1583362031349!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
-})
+
 
 //mostrar mapa barranco
 
-mapa16.addEventListener("click",()=>{
+mapa15.addEventListener("click",()=>{
     contenedorMapaSurco.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.393659531221!2d-76.97737994951959!3d-12.085180491399708!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c7ad784a96bd%3A0x8e0b3487046fc6f3!2sJuicy%20Lucy%20Jockey!5e0!3m2!1ses!2spe!4v1583362447251!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
 })
 
-mapa17.addEventListener("click",()=>{
+mapa16.addEventListener("click",()=>{
     contenedorMapaSurco.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.424744217262!2d-76.97090014951962!3d-12.083048191401115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c7ab38e317eb%3A0x84f82cc5c8668f9a!2sBon%20Beef%20Burger!5e0!3m2!1ses!2spe!4v1583362811439!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
 })
 
+//mostrar mapa san borja
 
+mapa17.addEventListener("click",()=>{
+    contenedorMapaBorja.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.2180141150207!2d-77.0135775853398!3d-12.097222191433744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c877e9e6001d%3A0xa04ed9bfe39d1076!2sQuimera%20Restaurante!5e0!3m2!1ses!2spe!4v1583370943869!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
 
+mapa18.addEventListener("click",()=>{
+    contenedorMapaBorja.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.315382319276!2d-77.00650488533992!3d-12.090548391438144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c7772ef78687%3A0x8a133b8beb3946a7!2sJuicy%20Lucy%20La%20Rambla!5e0!3m2!1ses!2spe!4v1583371153532!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+//mostrar mapa surquillo
+
+mapa19.addEventListener("click",()=>{
+    contenedorMapaSurquillo.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.060775201998!2d-77.02017068533972!3d-12.107991991426564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c951555c0d57%3A0xd37148e6ce0418be!2sTruck%20Park!5e0!3m2!1ses!2spe!4v1583371287740!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+//mostrar mapa victoria
+
+mapa20.addEventListener("click",()=>{
+    contenedorMapaVictoria.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.3376153096924!2d-77.02235008533995!3d-12.089023991439163!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c97da2ffa4cb%3A0x83aa1e16a0576108!2sJuicy%20Lucy%20-%20Santa%20Catalina!5e0!3m2!1ses!2spe!4v1583371863835!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
+
+//mostrar mapa cercado
+
+mapa21.addEventListener("click",()=>{
+    contenedorMapacercado.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7803.725966095885!2d-77.03967257030163!3d-12.052947000000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8c7cfd2f6d3%3A0x113be6779289e29d!2sRinc%C3%B3n%20Cervecero!5e0!3m2!1ses!2spe!4v1583372034014!5m2!1ses!2spe" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`
+})
